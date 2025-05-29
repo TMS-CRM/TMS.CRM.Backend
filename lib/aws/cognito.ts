3841e94758d55f8cc95ef7304d1ad1aea887b402
Leonardo Penz <leonardopenz99@gmail.com>
2025-05-29 22:18:22 +1200

    Fixing knex client env variable

--- lib/aws/cognito.ts
+++ lib/aws/cognito.ts
@@ -9,10 +9,6 @@ export function getCognitoClient(): CognitoIdentityProviderClient {
     _cognitoIdentityProviderClient = new CognitoIdentityProviderClient({
       endpoint: process.env.AWS_ENDPOINT,
       region: process.env.AWS_REGION,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-      },
     });
   }
 
--- lib/utils/knexClient.ts
+++ lib/utils/knexClient.ts
@@ -1,6 +1,11 @@
 import type { Knex } from 'knex';
 import knexFactory from 'knex';
+import { default as pg } from 'pg';
+import { toIsoFromPgTimestampType } from './dates.js';
 import { getSecret } from '../aws/secretsManager.js';
+
+/** `tms.crm.backend-stack.ts` will overwrite the `MIGRATIONS_DIR` env variable for the deploy, otherwise use the local path */
+const MIGRATIONS_DIR = process.env.MIGRATIONS_DIR ?? './knex/migrations';
 
 export type KnexTransaction = Knex.Transaction;
 export type KnexQueryBuilder = Knex.QueryBuilder;
@@ -18,7 +23,7 @@ function connectionPool(): Knex<any, unknown[]> {
     client: 'postgresql',
     connection: loadConfigSecret,
     migrations: {
-      directory: './knex/migrations',
+      directory: MIGRATIONS_DIR,
       tableName: 'knex_migrations',
       loadExtensions: ['.mjs'],
     },
@@ -47,3 +52,34 @@ async function loadConfigSecret(): Promise<object> {
     connectionString,
   };
 }
+
+// Make sure Postgres returns numbers as numbers
+pg.types.setTypeParser(pg.types.builtins.INT8, (value: string) => {
+  return value !== null ? parseInt(value) : null;
+});
+
+pg.types.setTypeParser(pg.types.builtins.NUMERIC, (value: string) => {
+  return value !== null ? parseFloat(value) : null;
+});
+
+// Make sure Postgres returns correctly-formatted dates
+pg.types.setTypeParser(pg.types.builtins.DATE, (value: string) => {
+  // By default, `pg` will try and parse the String as a JavaScript `Date`, causing it to add time and timezone information,
+  // while the database itself stores the date properly as a ISO 8601 date-only string (yyyy-mm-dd)
+  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+  return value !== null ? value : null;
+});
+
+pg.types.setTypeParser(pg.types.builtins.TIMESTAMP, (value: string) => {
+  // The database already contains the full Date information (without the timezone), but not quite in the proper ISO 8601 format
+  // (e.g. it will store `2024-02-11 14:09:16` instead of `2024-02-12T14:09:16`). Parsing to a JavaScript date will add
+  // timezone information back, so we want to treat it as a String
+  return value !== null ? toIsoFromPgTimestampType(value) : null;
+});
+
+pg.types.setTypeParser(pg.types.builtins.TIMESTAMPTZ, (value: string) => {
+  // The database already contains the full Date information (timezone included), but not quite in the proper ISO 8601 format
+  // (e.g. it will store `2024-02-11 14:09:16.545000 +00:00` instead of `2024-02-12T14:09:16.545Z`). We will return a parsed
+  // JavaScript date, which can subsequently be converted to an ISO String
+  return value !== null ? new Date(Date.parse(value)) : null;
+});
