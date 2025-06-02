b1f0e9bc24a48d4e082e5fa34123ce5a3702942c
Leonardo Penz <leonardopenz99@gmail.com>
2025-06-02 17:05:20 +1200

    Adding transaction

--- repositories/userTenantRepository.ts
+++ repositories/userTenantRepository.ts
@@ -1,3 +1,4 @@
+import type { Knex } from 'knex';
 import { knexClient } from '../lib/utils/knexClient.js';
 import { logger } from '../lib/utils/logger.js';
 import { UserTenant, type UserTenantDatabase } from '../models/entities/userTenant.js';
@@ -5,9 +6,10 @@ import { UserTenant, type UserTenantDatabase } from '../models/entities/userTena
 export const userTenantTableName = 'user_tenant';
 
 /** Insert the UserTenant */
-export async function insertUserTenant(userTenant: Partial<UserTenantDatabase>): Promise<number> {
-  const query = knexClient(userTenantTableName).insert(userTenant).returning('id');
-  const records = (await query) as UserTenantDatabase[];
+export async function insertUserTenant(userTenant: Partial<UserTenantDatabase>, transaction?: Knex.Transaction): Promise<number> {
+  const queryContext = transaction ? transaction(userTenantTableName) : knexClient(userTenantTableName);
+
+  const records = (await queryContext.insert(userTenant).returning('id')) as UserTenantDatabase[];
 
   logger.info(`Successfully inserted UserTenant. Id: ${records[0].id}`);
   return records[0].id;
